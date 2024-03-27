9631d6110e4019f451583f5084617bd170db846f
Sravan Balaji <balajsra@umich.edu>
2024-03-27 19:37:21 -0400

    Enable Prime Sync for System76 Oryx Pro 7

--- hosts/system76-oryx-pro-7.nix
+++ hosts/system76-oryx-pro-7.nix
@@ -30,7 +30,9 @@
   ##########################
   hardware.system76.enableAll = true;
   hardware.system76.kernel-modules.enable = true;
+
   services.xserver.videoDrivers = [ "nvidia" ];
+
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
@@ -38,5 +40,10 @@
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+    prime = {
+      sync.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
   };
 }
