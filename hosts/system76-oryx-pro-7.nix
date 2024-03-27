a2b1e0e55c3452b2821f827d40320a449a4cd807
Sravan Balaji <balajsra@umich.edu>
2024-03-27 18:45:36 -0400

    Add NVIDIA and System76 S/W manually

--- hosts/system76-oryx-pro-7.nix
+++ hosts/system76-oryx-pro-7.nix
@@ -24,4 +24,18 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  ##########################
+  # Manually Added Section #
+  ##########################
+  hardware.system76.enableAll = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
