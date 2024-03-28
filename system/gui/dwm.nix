a59fa6ec1ef640213a7eaf1779bd06ced443362e
Sravan Balaji <balajsra@umich.edu>
2024-03-27 20:26:37 -0400

    Attempt at switching from GNOME to dwm (not working)

--- system/gui/dwm.nix
+++ system/gui/dwm.nix
@@ -7,6 +7,25 @@
     ./x11.nix
   ];
 
+  nixpkgs = {
+    overlays = [
+      (self: super: {
+        dwm = super.dwm.overrideAttrs (oldattrs: {
+          src = fetchGit {
+            url = "https://gitea.sravanbalaji.com/sravan/dwm-flexipatch.git";
+            rev = "23b55eaf9cd9c08d4c14f7957675ae056e358e32";
+          };
+          buildInputs = with pkgs; [
+            xorg.libX11
+            xorg.libXinerama
+            xorg.libXft
+            yajl
+          ];
+        });
+      })
+    ];
+  };
+
   services.xserver.windowManager.dwm.enable = true;
   services.xserver.displayManager.defaultSession = "none+dwm";
 }
