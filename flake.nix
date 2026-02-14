4d8c828a2baf32bb1136d745bb76f9287e69d5ac
Sravan Balaji <sr98vn@gmail.com>
2026-02-14 09:09:00 -0500

    Enable SSH Server & Flake Cleanup
    - Enable SSH server
    - Set disko branch to master
    - Add some local variables to flake.nix for convenience
    - Update flake.lock

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
     description = "Flake of Sravan's NixOS";
 
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+        nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
         home-manager = {
             url = "github:nix-community/home-manager/release-25.11";
@@ -10,14 +11,20 @@
         };
 
         disko = {
-            url = "github:nix-community/disko";
+            url = "github:nix-community/disko/master";
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
 
-    outputs = { self, nixpkgs, home-manager, disko, ... }@inputs: {
-        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
+    outputs = { self, nixpkgs, home-manager, disko, ... }@inputs:
+    let
+        hostname = "nixos";
+        architecture = "x86_64-linux";
+        username = "sravan";
+    in
+    {
+        nixosConfigurations."${hostname}" = nixpkgs.lib.nixosSystem {
+            system = "${architecture}";
             specialArgs = { inherit inputs; };
             modules = [
                 disko.nixosModules.disko
@@ -27,7 +34,7 @@
                     home-manager = {
                         useGlobalPkgs = true;
                         useUserPackages = true;
-                        users.sravan = import ./home.nix;
+                        users."${username}" = import ./home.nix;
                         backupFileExtension = "backup";
                     };
                 }
