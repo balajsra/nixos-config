47bc82ca1074da481313f2f7eea56553db7f25c3
sravan <sr98vn@gmail.com>
2026-02-13 20:32:15 -0500

    LVM + LUKS + BTRFS Disko Setup
    - Setup disko to take arguments of which disks to span across and swap file size
    - Allocate 1 GB boot partition, then span rest of space on all disks to create LVM logical volume, encrypt it, and format with BTRFS with subvolumes

--- disko/lvm-luks-btrfs.nix
+++ disko/lvm-luks-btrfs.nix
@@ -1,77 +1,89 @@
+{ lib, ... }:
 let
-  installDisk = "sda";
-  bootPartitionSize = "512M";
-  swapfileSize = "10G";
-in {
-  disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/" + installDisk;
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-            };
-            ESP = {
-              size = bootPartitionSize;
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [
-                  "defaults"
-                ];
-              };
-            };
-            luks = {
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "cryptroot";
-                settings.allowDiscards = true;
-                content = {
-                  type = "btrfs";
-                  extraArgs = [ "-f" ];
-                  subvolumes = {
-                    "@root" = {
-                      mountpoint = "/";
-                      mountOptions = [ "compress=zstd" "noatime" ];
+    # --- Configuration Variables ---
+    myDisks = [ "/dev/sda" ];
+    swapSize = "16G";
+    # -------------------------------
+
+    # Helper to determine if a disk is the "primary" boot disk
+    isFirstDisk = dev: dev == (builtins.head myDisks);
+in
+{
+    disko.devices = {
+        disk = lib.genAttrs myDisks (dev: {
+            device = dev;
+            type = "disk";
+            content = {
+                type = "gpt";
+                partitions = {
+                    # Partition 1: Boot (Only on the first disk)
+                    boot = lib.mkIf (isFirstDisk dev) {
+                        size = "1G";
+                        type = "EF00";
+                        content = {
+                            type = "filesystem";
+                            format = "vfat";
+                            mountpoint = "/boot";
+                            mountOptions = [ "umask=0077" ];
+                        };
                     };
-                    "@home" = {
-                      mountpoint = "/home";
-                      mountOptions = [ "compress=zstd" "noatime" ];
+
+                    # Partition 2: LVM Physical Volume
+                    lvm = {
+                        size = "100%";
+                        content = {
+                            type = "lvm_pv";
+                            vg = "vgroot";
+                        };
                     };
-                    "@games" = {
-                      mountpoint = "/games";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "@nix" = {
-                      mountpoint = "/nix";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "@log" = {
-                      mountpoint = "/var/log";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                    "@swap" = {
-                      mountpoint = "/swap";
-                      swap.swapfile.size = swapfileSize;
-                    };
-                    "@.snapshots" = {
-                      mountpoint = "/.snapshots";
-                      mountOptions = [ "compress=zstd" "noatime" ];
-                    };
-                  };
                 };
-              };
             };
-          };
+        });
+
+        lvm_vg.vgroot = {
+            type = "lvm_vg";
+            lvs = {
+                lv_crypt_store = {
+                    size = "100%FREE";
+                    content = {
+                        type = "luks";
+                        name = "decrypted_root";
+                        extraFormatArgs = [
+                            "--type luks2"
+                            "--cipher aes-xts-plain64"
+                            "--key-size 512"
+                            "--hash sha512"
+                            "--pbkdf argon2id"
+                            "--iter-time 5000" # 5 seconds of hashing derivation
+                            "--use-random"
+                        ];
+                        settings = {
+                            allowDiscards = true; # Recommended for SSDs/NVMe
+                        };
+                        content = {
+                            type = "btrfs";
+                            extraArgs = [ "-f" ];
+                            subvolumes = {
+                                "@" = { mountpoint = "/"; mountOptions = [ "compress=zstd" ]; };
+                                "@home" = { mountpoint = "/home"; mountOptions = [ "compress=zstd" ]; };
+                                "@nix" = { mountpoint = "/nix"; mountOptions = [ "compress=zstd" "noatime" ]; };
+                                "@data" = { mountpoint = "/data"; mountOptions = [ "compress=zstd" ]; };
+                                "@tmp" = { mountpoint = "/tmp"; };
+                                "@var" = { mountpoint = "/var"; mountOptions = [ "compress=zstd" ]; };
+                                "@snapshots" = { mountpoint = "/.snapshots"; mountOptions = [ "compress=zstd" ]; };
+                                "@home_snapshots" = { mountpoint = "/home/.snapshots"; mountOptions = [ "compress=zstd" ]; };
+                                "@swap" = {
+                                    mountpoint = "/swap";
+                                    # Swap on Btrfs should not be compressed
+                                    mountOptions = [ "noatime" ];
+                                    swap.swapfile.size = swapSize;
+                                };
+                            };
+                        };
+                    };
+                };
+            };
         };
-      };
     };
-  };
 }
+		
