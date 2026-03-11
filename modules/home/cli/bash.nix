98890396557fa63b2659f3384b3ce1142ec876cc
Sravan Balaji <sr98vn@gmail.com>
2026-03-11 12:57:57 -0400

    Add bash alias for nixos-rebuild boot

--- modules/home/cli/bash.nix
+++ modules/home/cli/bash.nix
@@ -8,6 +8,7 @@ in
     shellAliases = {
       git-graph = "git log --all --decorate --oneline --graph";
       nix-switch = "git -C ${nixosConfigPath} add -N . && nixos-rebuild switch --flake ${nixosConfigPath}#$(hostname) --show-trace --sudo";
+      nix-boot = "git -C ${nixosConfigPath} add -N . && nixos-rebuild boot --flake ${nixosConfigPath}#$(hostname) --show-trace --sudo";
       nix-update = "nix flake update --flake ${nixosConfigPath}";
     };
   };
