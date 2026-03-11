a1a96bf93fb537122a6d0e62cc6b11083f2e91ff
Sravan Balaji <sr98vn@gmail.com>
2026-03-10 21:20:18 -0400

    Fix plymouth

--- modules/nixos/boot/plymouth.nix
+++ modules/nixos/boot/plymouth.nix
@@ -1,8 +1,25 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    plymouth
-    plymouth-matrix-theme
-  ];
+  # https://wiki.nixos.org/wiki/Plymouth
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "lone";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "lone" ];
+        })
+      ];
+    };
+
+    # Enable "silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+      "systemd.show_status=auto"
+    ];
+  };
 }
