c4e48b652dc97060be36899218df8fea1e2cd9ab
Sravan Balaji <sr98vn@gmail.com>
2026-03-12 10:42:00 -0400

    Add display configuration and theming packages to mangowm

--- modules/nixos/desktop/mangowm.nix
+++ modules/nixos/desktop/mangowm.nix
@@ -19,5 +19,13 @@
     xdg-desktop-portal
     xdg-desktop-portal-wlr
     xdg-desktop-portal-gtk
+    wlr-randr
+    wdisplays
+    shikane
+    nwg-look
+    kdePackages.qt6ct
+    kdePackages.qtstyleplugin-kvantum
+    breeze-hacked-cursor-theme
+    papirus-icon-theme
   ];
 }
