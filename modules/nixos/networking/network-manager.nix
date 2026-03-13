e237bff638471e5446089674abafde2787403ad5
Sravan Balaji <sr98vn@gmail.com>
2026-03-13 15:09:10 -0400

    Enable nm-applet, add user to networkmanager group, and prioritize ethernet over wifi in network manager nix

--- modules/nixos/networking/network-manager.nix
+++ modules/nixos/networking/network-manager.nix
@@ -1,3 +1,21 @@
+{ vars, ... }:
+
 {
-  networking.networkmanager.enable = true;
+  # https://wiki.nixos.org/wiki/NetworkManager
+  networking.networkmanager = {
+    enable = true;
+
+    # Prioritize Ethernet over WiFi
+    settings = {
+      "device" = {
+        "match-device" = "type:ethernet";
+        "ipv4.route-metric" = 100;
+        "ipv6.route-metric" = 100;
+      };
+    };
+  };
+
+  programs.nm-applet.enable = true;
+
+  users.users.${vars.username}.extraGroups = [ "networkmanager" ];
 }
