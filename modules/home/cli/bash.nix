7c60c58ecec209eb660c54b44a948e938aad2d0a
Sravan Balaji <sr98vn@gmail.com>
2026-03-13 14:25:42 -0400

    Update NixOS bash aliases to be prefixed with `ns` instead of `nix` and add a test dry-run command

--- modules/home/cli/bash.nix
+++ modules/home/cli/bash.nix
@@ -7,9 +7,10 @@ in
     enable = true;
     shellAliases = {
       git-graph = "git log --all --decorate --oneline --graph";
-      nix-switch = "git -C ${nixosConfigPath} add -N . && nixos-rebuild switch --flake ${nixosConfigPath}#$(hostname) --show-trace --sudo";
-      nix-boot = "git -C ${nixosConfigPath} add -N . && nixos-rebuild boot --flake ${nixosConfigPath}#$(hostname) --show-trace --sudo";
-      nix-update = "nix flake update --flake ${nixosConfigPath}";
+      ns-switch = "git -C ${nixosConfigPath} add -N . && nixos-rebuild switch --flake ${nixosConfigPath}#$(hostname) --show-trace --sudo";
+      ns-boot = "git -C ${nixosConfigPath} add -N . && nixos-rebuild boot --flake ${nixosConfigPath}#$(hostname) --show-trace --sudo";
+      ns-test = "git -C ${nixosConfigPath} add -N . && nixos-rebuild dry-activate --flake ${nixosConfigPath}#$(hostname) --show-trace --sudo";
+      ns-update = "nix flake update --flake ${nixosConfigPath}";
     };
   };
 
