a690a1e6bf605dab2a1301751f281f8334a97912
Sravan Balaji <sr98vn@gmail.com>
2026-04-05 15:19:30 -0400

    Enable sravan user aspect

--- modules/users/sravan.nix
+++ modules/users/sravan.nix
@@ -4,7 +4,7 @@ let
   username = "sravan";
 in
 {
-  flake.modules.nixos.${username} = {
+  flake.modules.nixos."${username}" = {
     # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.${username} = {
       isNormalUser = true;
@@ -12,7 +12,7 @@ in
     };
   };
 
-  flake.modules.homeManager.${username} =
+  flake.modules.homeManager."${username}" =
     { pkgs, lib, ... }:
     {
       home.username = lib.mkDefault username;
