32f20c62d56fc02116d15de1b1b79d1b0aabe4a8
Sravan Balaji <sr98vn@gmail.com>
2026-05-24 18:48:27 -0400

    Rename deprecated 'system' to 'stdenv.hostPlatform.system'

--- modules/hosts/proxmox-nix-vm.nix
+++ modules/hosts/proxmox-nix-vm.nix
@@ -29,7 +29,7 @@ in
           nixpkgs.overlays = [
             (final: prev: {
               stable = import inputs.nixpkgs-stable {
-                system = final.system;
+                system = final.stdenv.hostPlatform.system;
                 config = config.nixpkgs.config;
               };
             })
