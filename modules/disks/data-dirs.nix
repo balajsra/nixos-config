7eb357f3105bb6278d3f5e1316584f6cd3be5eba
Sravan Balaji <sr98vn@gmail.com>
2026-05-31 06:43:03 -0400

    Adjust group ownership of data-dirs and add symlinks for all top-levvel directories to home

--- modules/disks/data-dirs.nix
+++ modules/disks/data-dirs.nix
@@ -4,26 +4,27 @@
     { config, ... }:
     let
       user = config.primaryUser.username;
+      group = config.users.users.${user}.group;
     in
     {
       systemd.tmpfiles.rules = [
-        # Type  Path                                 Mode  User     Group  Age  Argument
-        "d      /data/${user}                        0700  ${user}  users  -    -"
-        "d      /data/${user}/Calibre-Library        0700  ${user}  users  -    -"
-        "d      /data/${user}/Documents              0700  ${user}  users  -    -"
-        "d      /data/${user}/Downloads              0700  ${user}  users  -    -"
-        "d      /data/${user}/Games                  0700  ${user}  users  -    -"
-        "d      /data/${user}/Games/EA-App           0700  ${user}  users  -    -"
-        "d      /data/${user}/Games/Heroic           0700  ${user}  users  -    -"
-        "d      /data/${user}/Games/Prism-Launcher   0700  ${user}  users  -    -"
-        "d      /data/${user}/Games/Steam            0700  ${user}  users  -    -"
-        "d      /data/${user}/Games/Ubisoft-Connect  0700  ${user}  users  -    -"
-        "d      /data/${user}/Music                  0700  ${user}  users  -    -"
-        "d      /data/${user}/NextCloud              0700  ${user}  users  -    -"
-        "d      /data/${user}/Pictures               0700  ${user}  users  -    -"
-        "d      /data/${user}/Projects               0700  ${user}  users  -    -"
-        "d      /data/${user}/Second-Brain           0700  ${user}  users  -    -"
-        "d      /data/${user}/Videos                 0700  ${user}  users  -    -"
+        # Type  Path                                 Mode  User     Group     Age  Argument
+        "d      /data/${user}                        0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Calibre-Library        0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Documents              0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Downloads              0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Games                  0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Games/EA-App           0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Games/Heroic           0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Games/Prism-Launcher   0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Games/Steam            0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Games/Ubisoft-Connect  0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Music                  0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/NextCloud              0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Pictures               0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Projects               0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Second-Brain           0700  ${user}  ${group}  -    -"
+        "d      /data/${user}/Videos                 0700  ${user}  ${group}  -    -"
       ];
     };
 
@@ -34,6 +35,17 @@
     in
     {
       home.file."Data".source = config.lib.file.mkOutOfStoreSymlink "/data/${user}";
+      home.file."Calibre-Library".source =
+        config.lib.file.mkOutOfStoreSymlink "/data/${user}/Calibre-Library";
+      home.file."Documents".source = config.lib.file.mkOutOfStoreSymlink "/data/${user}/Documents";
+      home.file."Downloads".source = config.lib.file.mkOutOfStoreSymlink "/data/${user}/Downloads";
+      home.file."Games".source = config.lib.file.mkOutOfStoreSymlink "/data/${user}/Games";
+      home.file."Music".source = config.lib.file.mkOutOfStoreSymlink "/data/${user}/Music";
+      home.file."NextCloud".source = config.lib.file.mkOutOfStoreSymlink "/data/${user}/NextCloud";
+      home.file."Pictures".source = config.lib.file.mkOutOfStoreSymlink "/data/${user}/Pictures";
+      home.file."Projects".source = config.lib.file.mkOutOfStoreSymlink "/data/${user}/Projects";
+      home.file."Second-Brain".source = config.lib.file.mkOutOfStoreSymlink "/data/${user}/Second-Brain";
+      home.file."Videos".source = config.lib.file.mkOutOfStoreSymlink "/data/${user}/Videos";
 
       xdg.userDirs = {
         enable = true;
