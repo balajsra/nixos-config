1a080a419d8a12ac00404fe7ef0dd71dcdeabc7b
Sravan Balaji <sr98vn@gmail.com>
2026-06-21 15:14:06 +0000

    Update nix-secrets and add powerspec host

---
+++ modules/hosts/powerspec.nix
@@ -0,0 +1,216 @@
+{
+  self,
+  inputs,
+  withSystem,
+  ...
+}:
+let
+  hostname = "powerspec";
+  timezone = "America/New_York";
+  architecture = "x86_64-linux";
+in
+{
+  flake.nixosConfigurations."${hostname}" = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      self.nixosModules."${hostname}-configuration"
+      self.nixosModules."${hostname}-hardware"
+      self.nixosModules.variables
+      self.nixosModules.home-manager
+      (
+        { config, lib, ... }:
+        {
+          nixpkgs.hostPlatform = lib.mkDefault "${architecture}";
+          # Unfree Software: https://nixos.wiki/wiki/Unfree_Software
+          nixpkgs.config.allowUnfree = true;
+
+          # Modify pkgs to include a `stable` keyword to reference stable package repo
+          # Unstable packages installed as `pkgs.<package-name>`
+          # Stable packages installed as `pkgs.stable.<package-name>`
+          nixpkgs.overlays = [
+            (final: prev: {
+              stable = import inputs.nixpkgs-stable {
+                system = final.stdenv.hostPlatform.system;
+                config = config.nixpkgs.config;
+              };
+            })
+            inputs.dracula-signal-desktop.overlays
+          ];
+
+          storage = {
+            lvm-luks-btrfs = {
+              enable = true;
+              osDisks = [
+                "/dev/nvme0n1"
+              ];
+              swapSize = "4G";
+            };
+          };
+
+          features = {
+            boot = {
+              grub-luks-btrfs.enable = true;
+              plymouth.enable = true;
+              kernel = "vanilla-latest";
+            };
+
+            display-manager = "greetd";
+            desktop-environment = "mango";
+
+            terminal = {
+              bash.enable = true;
+              fish.enable = true;
+              emulator = "ghostty";
+            };
+
+            networking = {
+              ssh-server.enable = false;
+              ssh-client.enable = true;
+              vpn = {
+                enable = true;
+                home = false;
+                proton = true;
+              };
+            };
+
+            file-sharing = {
+              nextcloud.enable = true;
+              syncthing.enable = true;
+              samba-client = {
+                enable = true;
+                fileserver.enable = true;
+                mediaserver.enable = true;
+              };
+            };
+
+            hardware = {
+              system76.enable = false;
+            };
+
+            comms.enable = true;
+
+            fonts = {
+              enable = true;
+              nerd.enable = true;
+              emojis.enable = true;
+              japanese.enable = true;
+              korean.enable = true;
+            };
+          };
+
+          primaryUser = {
+            name = "Sravan Balaji";
+            email = "sr98vn@gmail.com";
+            username = "sravan";
+            nixosConfigPath = /home/${config.primaryUser.username}/.config/nixos;
+            dotfilesPath = /home/${config.primaryUser.username}/.config/nixos/dotfiles;
+          };
+
+          imports = [ self.nixosModules.admin ];
+          home-manager.users."${config.primaryUser.username}" = {
+            imports = [
+              self.homeModules.admin
+              self.homeModules.comms
+              self.homeModules.data-dirs
+              self.homeModules.desktop-environment
+              self.homeModules.development
+              self.homeModules.editor
+              self.homeModules.file-sharing
+              self.homeModules.gaming
+              self.homeModules.git
+              self.homeModules.media
+              self.homeModules.networking
+              self.homeModules.night-light
+              self.homeModules.office
+              self.homeModules.phone
+              self.homeModules.security
+              self.homeModules.terminal
+              self.homeModules.web-browser
+            ];
+          };
+        }
+      )
+    ];
+  };
+
+  flake.nixosModules."${hostname}-configuration" =
+    { pkgs, ... }:
+    {
+      imports = [
+        self.nixosModules.bluetooth
+        self.nixosModules.boot-animation
+        self.nixosModules.boot-loader
+        self.nixosModules.data-dirs
+        self.nixosModules.desktop-environment
+        self.nixosModules.display-manager
+        self.nixosModules.editor
+        self.nixosModules.file-sharing
+        self.nixosModules.fonts
+        self.nixosModules.gaming
+        self.nixosModules.git
+        self.nixosModules.kernel
+        self.nixosModules.location
+        self.nixosModules.networking
+        self.nixosModules.night-light
+        self.nixosModules.office
+        self.nixosModules.partitions
+        self.nixosModules.phone
+        self.nixosModules.printing
+        self.nixosModules.removable-media
+        self.nixosModules.security
+        self.nixosModules.sound
+        self.nixosModules.system76
+        self.nixosModules.utils
+      ];
+
+      networking.hostName = "${hostname}";
+      time.timeZone = "${timezone}";
+
+      # Run unpatched dynamic binaries on NixOS
+      programs.nix-ld.enable = true;
+
+      nix = {
+        settings.experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+
+        # Storage Optimization: https://nixos.wiki/wiki/Storage_optimization
+        # Optimising the store
+        optimise = {
+          automatic = true;
+          dates = [ "01:00" ]; # Daily at 1:00 AM (or next boot)
+        };
+
+        # Garbage Collection
+        gc = {
+          automatic = true;
+          dates = "weekly";
+          options = "--delete-older-than 30d";
+        };
+      };
+
+      # Do not change, this is a safety anchor to prevent
+      # system from breaking or losing data during an upgrade
+      system.stateVersion = "25.11";
+    };
+
+  flake.nixosModules."${hostname}-hardware" =
+    {
+      modulesPath,
+      lib,
+      config,
+      pkgs,
+      ...
+    }:
+    {
+      imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+      
+      boot.initrd.availableKernelModules = [ "nvme" "ahci" "xhci_pci" "usb_storage" "usbhid" "uas" "sd_mod" ];
+      boot.initrd.kernelModules = [ "dm-snapshot" ];
+      boot.kernelModules = [ "kvm-amd" ];
+      boot.extraModulePackages = [ ];
+      
+      nixpkgs.hostPlatform = lib.mkDefault "${architecture}";
+      hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    };
+}
