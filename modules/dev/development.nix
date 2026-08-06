5fd5567d1c4a0f354c93b7951057e167d362dc25
Sravan Balaji <sr98vn@gmail.com>
2026-08-05 22:08:43 -0400

    Fix fish shell and devenv interaction

--- modules/dev/development.nix
+++ modules/dev/development.nix
@@ -32,8 +32,21 @@
 
       programs.fish = lib.mkIf osConfig.features.terminal.fish.enable {
         interactiveShellInit = ''
-          # Devenv Auto Activation
-          devenv hook fish | source
+          # Wrap existing fish_greeting if it hasn't been wrapped yet
+          if functions -q fish_greeting; and not functions -q __base_fish_greeting
+            functions -c fish_greeting __base_fish_greeting
+
+            function fish_greeting
+              if not set -q DEVENV_ROOT; and not set -q DEVENV_STATE; and not set -q DIRENV_DIR
+                __base_fish_greeting
+              end
+            end
+          end
+
+          # Only run devenv hook in top-level shells, not inside devenv subshells
+          if not set -q DEVENV_ROOT
+            devenv hook fish | source
+          end
         '';
       };
     };
