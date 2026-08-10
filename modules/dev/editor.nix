fc5bf3e3bc93fdb246373ea5f0f9df32d877413a
Sravan Balaji <sr98vn@gmail.com>
2026-08-10 23:02:59 +0000

    Add pdf viewer extension to vscode

--- modules/dev/editor.nix
+++ modules/dev/editor.nix
@@ -86,6 +86,7 @@
               ms-vscode-remote.remote-ssh-edit
               ms-vscode.remote-explorer
               nefrob.vscode-just-syntax
+              tomoki1207.pdf
               vscodevim.vim
             ];
 
