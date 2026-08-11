0887e25367c8a418167f598f0356ade76df9aea2
Sravan Balaji <sr98vn@gmail.com>
2026-08-11 02:10:54 +0000

    Turn dracula pro vim theme into a custom package

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,12 @@
 inputs: final: prev: {
   dracula-gtk-theme = final.callPackage ./dracula-gtk-theme.nix { };
 
+  vimPlugins = prev.vimPlugins // {
+    dracula-pro = final.callPackage ./vimPlugins/dracula-pro.nix {
+      src = inputs.dracula-pro-vim;
+    };
+  };
+
   vscode-extensions = prev.vscode-extensions // {
     datakurre = (prev.vscode-extensions.datakurre or { }) // {
       devenv = final.callPackage ./vscode-extensions/datakurre-devenv.nix { };
