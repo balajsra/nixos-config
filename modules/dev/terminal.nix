e281d52d70143798d0b35f22f67a13c2d3578bd5
Sravan Balaji <sr98vn@gmail.com>
2026-08-12 23:12:20 +0000

    Remove unnecessary color specification in tmux dracula plugin config

--- modules/dev/terminal.nix
+++ modules/dev/terminal.nix
@@ -391,20 +391,6 @@
                 set -g @dracula-show-left-icon session
                 set -g @dracula-show-empty-plugins false
                 set -g @dracula-plugins "git cpu-usage ram-usage battery time"
-
-                set -g @dracula-colors "
-                  white=\"#f8f8f2\"
-                  gray=\"#44475a\"
-                  dark_gray=\"#282a36\"
-                  light_purple=\"#bd93f9\"
-                  dark_purple=\"#6272a4\"
-                  cyan=\"#8be9fd\"
-                  green=\"#50fa7b\"
-                  orange=\"#ffb86c\"
-                  red=\"#ff5555\"
-                  pink=\"#ff79c6\"
-                  yellow=\"#f1fa8c\"
-                "
               '';
             }
           ];
