7f0b29cd78aae88692fff52df5a5ac780e940b72
Sravan Balaji <sr98vn@gmail.com>
2026-08-13 01:49:07 +0000

    Patch fallout plymouth theme to fix script

--- pkgs/fallout-plymouth-theme.nix
+++ pkgs/fallout-plymouth-theme.nix
@@ -18,13 +18,26 @@ stdenv.mkDerivation {
   dontBuild = true;
 
   installPhase = ''
-    mkdir -p $out/share/plymouth/themes/fallout
-    cp -r * $out/share/plymouth/themes/fallout/
-    cd $out/share/plymouth/themes/fallout
+    runHook preInstall
 
-    # Fix hardcoded path in .plymouth
+    target_dir="$out/share/plymouth/themes/fallout"
+    mkdir -p "$target_dir"
+    cp -r * "$target_dir/"
+    cd "$target_dir"
+
+    # Patch absolute paths in fallout.plymouth to point to $out
     substituteInPlace fallout.plymouth \
-      --replace-fail "/usr/share/plymouth/themes/fallout" "$out/share/plymouth/themes/fallout"
+      --replace-fail "/usr/share/plymouth/themes/fallout" "$target_dir"
+
+    # Inject missing global declarations at the top of fallout.script
+    #   - RotatedImageCache = []; prevents array indexing runtime crash
+    #   - motif definitions prevent null reference crashes in callbacks
+    sed -i '1s/^/RotatedImageCache = [];\nmotif = [];\nmotif.sprite = SpriteNew();\nmotif.opacity = 0;\n/' fallout.script
+
+    # Comment out lingering motif calls that attempt to set properties on null
+    sed -i 's/motif\.sprite\.SetOpacity/\/\/ motif.sprite.SetOpacity/g' fallout.script
+
+    runHook postInstall
   '';
 
   meta = with lib; {
