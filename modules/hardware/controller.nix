e411726c0bb9e7d23f4e513eceb8b815d4267b36
Sravan Balaji <sr98vn@gmail.com>
2026-09-05 22:58:33 +0000

    Fix Xbox controller bluetooth input issues

--- modules/hardware/controller.nix
+++ modules/hardware/controller.nix
@@ -18,6 +18,25 @@
     {
       config = lib.mkIf (config.features.hardware.controller.xbox.enable) {
         hardware.xpadneo.enable = true;
+
+        # Low-level Bluetooth stability & power management tweaks
+        boot.extraModprobeConfig = ''
+          # Enhanced Retransmission Mode (ERTM) is a known protocl bug in Linux's Bluetooth stack for Xbox controllers
+          # Without this, the controller will randomly drop its Bluetooth connection or fail to re-pair
+          # automatically after going to sleep
+          options bluetooth disable_ertm=1
+
+          # Disable auto-suspending bluetooth radio
+          options btusb enable_autosuspend=0
+        '';
+
+        # Environment overrides for Steam / SDL evdev passthrough
+        environment.sessionVariables = {
+          # Forces SDL to use standard Linux evdev instead of HIDAPI for Xbox gamepads
+          SDL_JOYSTICK_HIDAPI_XBOX = "0";
+          # Tells steam to disable its low-level hidraw hook for Xbox gamepads
+          STEAM_DISABLE_HIDRAW_XBOX = "1";
+        };
       };
     };
 
